115c6b0997c5d8d83f147e550872f108d88d6abd
HernanCamiloYate <57019871+HernanCamiloYate@users.noreply.github.com>
2019-11-26 10:31:48 -0500

    Create CalcularEdad.R

---
+++ CodigosR/CalcularEdad.R
@@ -0,0 +1,12 @@
+# Calcula la edad en años a partir de la fecha de nacimiento y una fecha de referencia
+
+Calcular.Edad = function(from, to) {
+  from_lt = as.POSIXlt(from)
+  to_lt = as.POSIXlt(to)
+  
+  age = to_lt$year - from_lt$year
+  
+  ifelse(to_lt$mon < from_lt$mon |
+           (to_lt$mon == from_lt$mon & to_lt$mday < from_lt$mday),
+         age - 1, age)
+}
