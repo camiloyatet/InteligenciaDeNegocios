149226a2af4e4fbc1dec88500a2b6779ae7c6545
HernanCamiloYate <57019871+HernanCamiloYate@users.noreply.github.com>
2019-11-26 10:27:14 -0500

    Update and rename Convert Account Format.R to ConvertirContabilidad.R

--- CodigosR/ConvertirContabilidad.R
+++ CodigosR/ConvertirContabilidad.R
@@ -1,3 +1,5 @@
+# Convierte una cadena de carateres con formato contabilidad en número
+
 convertir.contabilidad <- function(x){
   if(grepl("\\(.*\\)", x)){
     as.numeric(paste0("-", gsub("\\(|\\)", "", gsub("[\\$, ]", "", x))))
