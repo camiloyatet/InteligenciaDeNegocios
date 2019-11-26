00db39168ba8485bb7b23018a51fcf710aec4307
HernanCamiloYate <57019871+HernanCamiloYate@users.noreply.github.com>
2019-11-26 10:29:05 -0500

    Update and rename Load Packages.R to CargarPaquetes.R

--- CodigosR/CargarPaquetes.R
+++ CodigosR/CargarPaquetes.R
@@ -1,3 +1,5 @@
+# Verifica si un paquete esta instalado, de no ser asi lo instala y los carga a la sesion vigente
+
 Loadpkg <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
