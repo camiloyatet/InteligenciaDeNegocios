61d49fd07fc8b3f003e66f424a3c63a3d7fdf464
HernanCamiloYate <57019871+HernanCamiloYate@users.noreply.github.com>
2020-02-13 10:55:02 -0500

    Add files via upload

---
+++ Aplicaciones Shiny/Guia Salarial/global.R
@@ -0,0 +1,18 @@
+options(scipen = 99999)
+
+library(shinydashboard)
+library(tidyverse)
+library(data.table)
+library(DT)
+library(shinydashboardPlus)
+library(shinythemes)
+library(shinyjs)
+library(dashboardthemes)
+library(scales)
+library(rhandsontable)
+
+data <- readRDS("data/data.rds")
+
+
+
+
